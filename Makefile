2d574d797e8c5fb4176d939119fd482a51a6df0a
Geraint <luffgd@gmail.com>
2020-10-05 22:03:38 +0100

    Add FFTW benchmarks

--- Makefile
+++ Makefile
@@ -5,6 +5,9 @@ endif
 
 SHARED_PATH := "geraint"
 
+clean:
+	rm -rf out
+
 ############## Testing ##############
 
 TEST_CPP_FILES := $(shell find tests -iname "*.cpp" | sort)
@@ -21,39 +24,32 @@ out/test: *.h $(shell find tests -iname "*.h") $(shell find tests -iname "*.cpp"
 		-I tests/ ${TEST_CPP_FILES} \
 		-o out/test
 
-############## Benchmark ##############
+############## Benchmarking ##############
 
-BENCHMARK_CPP_FILES := benchmark/benchmark.cpp
+graphs:
+	python benchmark/graphs.py
 
-benchmark: test out/benchmark
+benchmark: test benchmark-main benchmark-kissfft benchmark-previous benchmark-dev-history
+
+BENCHMARK_TEST_TIME := 0.05
+
+# Generic versions
+
+benchmark-%: out/benchmark-%
 	mkdir -p out/results
-	cd out && ./benchmark --test-time=0.01
+	cd out && ./benchmark-$* --test-time=${BENCHMARK_TEST_TIME}
 
-out/benchmark: *.h $(shell find benchmark -iname "*.h") $(shell find benchmark -iname "*.cpp")
-	echo "building benchmark: ${BENCHMARK_CPP_FILES}"
-	mkdir -p out
-	g++ -std=c++11 -msse2 -mavx -g -O3 \
-		"${SHARED_PATH}/test/main.cpp" -I "${SHARED_PATH}" \
-		-I benchmark/ ${BENCHMARK_CPP_FILES} \
-		-o out/benchmark
-
-############## Benchmark against others ##############
-
-BENCHMARK_ALL_CPP_FILES := benchmark/*.cpp
-
-benchmark-all: out/benchmark-all
-	mkdir -p out/results
-	cd out && ./benchmark-all --test-time=0.02
-
-out/benchmark-all: *.h $(shell find benchmark -iname "*.h") $(shell find benchmark -iname "*.cpp")
-	echo "building benchmark: ${BENCHMARK_ALL_CPP_FILES}"
+out/benchmark-%: *.h $(shell find benchmark -iname "*.h") $(shell find benchmark -iname "*.cpp")
 	mkdir -p out
 	g++ -std=c++11 -msse2 -mavx -Wfatal-errors -g -O3 \
 		"${SHARED_PATH}/test/main.cpp" -I "${SHARED_PATH}" \
-		-I benchmark/ ${BENCHMARK_ALL_CPP_FILES} \
-		-o out/benchmark-all
+		-I benchmark/ benchmark/$*.cpp \
+		-o out/benchmark-$*
 
-############## Clean ##############
-
-clean:
-	rm -rf out
+out/benchmark-fftw: $(shell find benchmark -iname "*.h") $(shell find benchmark -iname "*.cpp")
+	mkdir -p out
+	g++ -std=c++11 -msse2 -mavx -Wfatal-errors -g -O3 \
+		"${SHARED_PATH}/test/main.cpp" -I "${SHARED_PATH}" \
+		-I benchmark/ benchmark/fftw.cpp \
+		-lfftw3 \
+		-o out/benchmark-fftw
--- benchmark/benchmark.h
+++ benchmark/benchmark.h
@@ -1,16 +1,33 @@
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
+#include <algorithm>
 
 #include "../tests/tests-common.h"
 
+template<typename T>
+class OutOfPlaceRunner {
+	std::vector<std::complex<T>> inVector, outVector;
+public:
+	size_t size;
+
+	OutOfPlaceRunner(size_t size) {}
+	virtual ~OutOfPlaceRunner() {}
+	
+	virtual void getPointers(std::complex<T> **inPointer, std::complex<T> **outPointer) {
+		inVector.resize(size);
+		outVector.resize(size);
+		*inPointer = inVector.data();
+		*outPointer = outVector.data();
+	}
+};
+
 template<typename Implementation, int constMaxSize=INT_MAX>
 struct Benchmark {
-	static std::vector<int> getSizes() {
+	static std::vector<int> getSizes(int customMaxSize=16777216) {
 		std::vector<int> possibleSizes(0);
 		int size = 1;
-		int maxSize = std::min(constMaxSize, (1<<24));
-		// maxSize = std::min(constMaxSize, (1<<16));
+		int maxSize = std::min(constMaxSize, customMaxSize);
 		while (size <= maxSize) {
 			possibleSizes.push_back(size);
 			size *= 2;
@@ -25,7 +42,6 @@ struct Benchmark {
 			std::vector<int> mults = {3, 5, 7, 9, 11, 15, 23};
 			mults = {3, 5, 9, 15, 25};
 			mults = {3, 9};
-			// mults = {3,5,7,9,11,13,15,17,19,21,23,25,27,29};
 			for (int mult : mults) {
 				if (size*mult < maxSize) {
 					result.push_back(size*mult);
@@ -36,46 +52,55 @@ struct Benchmark {
 	}
 
 	template <typename Runner>
-	static void runBenchmark(Test &test, std::string name, std::string resultFile) {
+	static void runBenchmark(Test &test, std::string name, std::string resultPrefix) {
 		auto sizes = getSizes();
+		std::sort(sizes.begin(), sizes.end());
 
-		for (size_t i = 0; i < name.size(); i++) std::cout << "-";
-		std::cout << "-\t";
-		BenchmarkRate::print(sizes);
-		std::cout << name << ":\n\t";
+		std::cout << name << ":\n";
 		std::vector<double> rates = BenchmarkRate::map<int>(sizes, [](int size, int repeats, Timer &timer) {
-			timer.scaleRate(std::max(1.0, size*log(size))*1e-6);
-
 			auto runner = Runner(size);
 
-			std::vector<std::complex<double>> input(size);
-			std::vector<std::complex<double>> output(size);
+			// The runner handles input/output allocation
+			std::complex<double> *input, *output;
+			runner.getPointers(&input, &output);
+
 			for (int i = 0; i < size; i++) {
 				input[i] = randomComplex<double>();
 			}
 
 			timer.start();
 			for (int repeat = 0; repeat < repeats; ++repeat) {
-				runner.forward(input.data(), output.data());
+				runner.forward(input, output);
 			}
 			timer.stop();
 		}, true);
+		std::vector<double> scaled = rates;
+		for (size_t i = 0; i < sizes.size(); i++) {
+			double size = sizes[i];
+			double scaling = std::max(1.0, size*log(size))*1e-6;
+			scaled[i] = rates[i]*scaling;
+		}
 
-		std::ofstream output;
-		output.open(resultFile);
+		std::ofstream outputJs, outputCsv;
+		outputJs.open(resultPrefix + ".js");
+		outputCsv.open(resultPrefix + ".csv");
 
-              output << "addResults(\"" << name << "\", [";
-              for (size_t i = 0; i < sizes.size(); i++) {
-              	if (i > 0) output << ",";
-              	output << "\n\t{size: " << sizes[i] << ", rate: " << rates[i] << "}";
-              }
-              output << "\n]);";
+		outputJs << "addResults(\"" << name << "\", [";
+		outputCsv << "size,ops/sec," << name << "\n";
+		outputCsv.precision(15);
+
+		for (size_t i = 0; i < sizes.size(); i++) {
+			if (i > 0) outputJs << ",";
+			outputJs << "\n\t{size: " << sizes[i] << ", rate: " << scaled[i] << "}";
+			outputCsv << sizes[i] << "," << rates[i] << "," << scaled[i] << "\n";
+		}
+		outputJs << "\n]);";
 
 		return test.pass();
 	}
 
 	TEST_METHOD("double out-of-place", double_out) {
-		std::string resultFile = std::string("results/") + Implementation::resultTag() + ".js";
-		runBenchmark<typename Implementation::DoubleOutOfPlace>(test, Implementation::name(), resultFile);
+		std::string resultPrefix = std::string("results/") + Implementation::resultTag();
+		runBenchmark<typename Implementation::DoubleOutOfPlace>(test, Implementation::name(), resultPrefix);
 	}
 };
