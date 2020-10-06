e4ae456ca63914292a00ee78c6852feec8ea485b
Geraint <luffgd@gmail.com>
2020-10-06 19:59:11 +0100

    Direct calculation

--- benchmark/dev-history.cpp
+++ benchmark/dev-history.cpp
@@ -16,9 +16,9 @@ struct DevHistory {
 	}
 
 	template <typename T>
-	struct OutOfPlace {
+	struct OutOfPlace : public OutOfPlaceRunner<T> {
 		Historical<T> fft;
-		OutOfPlace(size_t size) : fft(size) {}
+		OutOfPlace(size_t size) : OutOfPlaceRunner<T>(size), fft(size) {}
 
 		void forward(std::complex<T> *input, std::complex<T> *output) {
 			fft.fft(input, output);
@@ -28,27 +28,9 @@ struct DevHistory {
 	using DoubleOutOfPlace = OutOfPlace<double>;
 };
 
+#undef SIGNALSMITH_FFT_V5
+#undef SIGNALSMITH_FFT_NAMESPACE
+#define SIGNALSMITH_FFT_NAMESPACE signalsmith_direct
 #include "dev-history/direct.h"
 char directName[] = "Direct", directTag[] = "dev-history-direct";
-Benchmark<DevHistory<dev_direct::FFT, directName, directTag>, 256> benchDirect;
-
-#include "dev-history/unspecialised-factors.h"
-char unspecialisedName[] = "Unspecialised", unspecialisedTag[] = "dev-history-unspecialised";
-Benchmark<DevHistory<dev_unspecialised_factors::FFT, unspecialisedName, unspecialisedTag>, (1<<16)> benchUnspecialised;
-
-#include "dev-history/specialised-23.h"
-char specialised23Name[] = "Radix-2/3", specialised23Tag[] = "dev-history-specialised23";
-Benchmark<DevHistory<dev_specialised23::FFT, specialised23Name, specialised23Tag>, (1<<16)> benchSpecialised23;
-
-#include "dev-history/specialised-235.h"
-char specialised235Name[] = "Radix-2/3/5", specialised235Tag[] = "dev-history-specialised235";
-Benchmark<DevHistory<dev_specialised235::FFT, specialised235Name, specialised235Tag>, (1<<20)> benchSpecialised235;
-
-#include "dev-history/memory-ping-pong.h"
-char pingpongName[] = "Ping-pong", pingpongTag[] = "dev-history-ping-pong";
-Benchmark<DevHistory<dev_memory_pingpong::FFT, pingpongName, pingpongTag>, (1<<20)> benchPingPong;
-
-#include "dev-history/repeated-twiddles.h"
-char repeatedTwiddlesName[] = "Repeated twiddles", repeatedTwiddlesTag[] = "dev-history-repeated-twiddles";
-Benchmark<DevHistory<dev_repeated_twiddles::FFT, repeatedTwiddlesName, repeatedTwiddlesTag>, (1<<20)> benchRepeatedTwiddles;
-
+Benchmark<DevHistory<signalsmith_direct::FFT, directName, directTag>, 256> benchDirect;
